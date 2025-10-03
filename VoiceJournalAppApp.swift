4ddb093968d96b2835105d5f9f71061a5249bce7
Abhit Sharma <abhits05@gmail.com>
2025-10-03 14:11:57 +0530

    Initial commit

---
+++ VoiceJournalAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceJournalAppApp.swift
+//  VoiceJournalApp
+//
+//  Created by Abhit Sharma on 03/10/25.
+//
+
+import SwiftUI
+
+@main
+struct VoiceJournalAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
